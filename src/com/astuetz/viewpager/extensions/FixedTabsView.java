79a0c838dfb032796814824dce0c8e8e940b9e98
Tonic Artos <tonic.artos@gmail.com>
2013-01-16 16:06:16 +1300

    Add support for a OnPageChangeListener passthrough.

--- src/com/astuetz/viewpager/extensions/FixedTabsView.java
+++ src/com/astuetz/viewpager/extensions/FixedTabsView.java
@@ -22,71 +22,97 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
 
 public class FixedTabsView extends LinearLayout implements ViewPager.OnPageChangeListener {
-	
+
 	@SuppressWarnings("unused")
 	private static final String TAG = "com.astuetz.viewpager.extensions";
-	
+	private static final OnPageChangeListener mDummyPageChangeListener = new OnPageChangeListener() {
+		@Override
+		public void onPageSelected(int arg0) {
+		}
+
+		@Override
+		public void onPageScrolled(int arg0, float arg1, int arg2) {
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int arg0) {
+		}
+	};
+
+	OnPageChangeListener mOnPageChangeListener = mDummyPageChangeListener;
+
 	private Context mContext;
-	
+
 	private ViewPager mPager;
-	
+
 	private TabsAdapter mAdapter;
-	
+
 	private ArrayList<View> mTabs = new ArrayList<View>();
-	
+
 	private Drawable mDividerDrawable;
-	
+
 	private int mDividerColor = 0xFF636363;
 	private int mDividerMarginTop = 12;
 	private int mDividerMarginBottom = 21;
-	
+
 	public FixedTabsView(Context context) {
 		this(context, null);
 	}
-	
+
 	public FixedTabsView(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
-	
+
 	public FixedTabsView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs);
-		
+
 		this.mContext = context;
-		
+
 		final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ViewPagerExtensions, defStyle, 0);
-		
+
 		mDividerColor = a.getColor(R.styleable.ViewPagerExtensions_dividerColor, mDividerColor);
-		
+
 		mDividerMarginTop = a.getDimensionPixelSize(R.styleable.ViewPagerExtensions_dividerMarginTop, mDividerMarginTop);
-		mDividerMarginBottom = a.getDimensionPixelSize(R.styleable.ViewPagerExtensions_dividerMarginBottom,
-		    mDividerMarginBottom);
-		
+		mDividerMarginBottom = a.getDimensionPixelSize(R.styleable.ViewPagerExtensions_dividerMarginBottom, mDividerMarginBottom);
+
 		mDividerDrawable = a.getDrawable(R.styleable.ViewPagerExtensions_dividerDrawable);
-		
+
 		a.recycle();
-		
+
 		this.setOrientation(LinearLayout.HORIZONTAL);
 	}
-	
+
+	public void setOnPageChangeListener(OnPageChangeListener listener) {
+		if (listener == null) {
+			mOnPageChangeListener = mDummyPageChangeListener;
+			return;
+		}
+
+		mOnPageChangeListener = listener;
+	}
+
 	/**
 	 * Sets the data behind this FixedTabsView.
 	 * 
 	 * @param adapter
-	 *          The {@link TabsAdapter} which is responsible for maintaining the
-	 *          data backing this FixedTabsView and for producing a view to
-	 *          represent an item in that data set.
+	 *            The {@link TabsAdapter} which is responsible for maintaining
+	 *            the data backing this FixedTabsView and for producing a view
+	 *            to represent an item in that data set.
 	 */
 	public void setAdapter(TabsAdapter adapter) {
 		this.mAdapter = adapter;
-		
-		if (mPager != null && mAdapter != null) initTabs();
+
+		if (mPager != null && mAdapter != null) {
+			initTabs();
+		}
 	}
-	
+
 	/**
 	 * Binds the {@link ViewPager} to this View
 	 * 
@@ -94,59 +120,67 @@ public class FixedTabsView extends LinearLayout implements ViewPager.OnPageChang
 	public void setViewPager(ViewPager pager) {
 		this.mPager = pager;
 		mPager.setOnPageChangeListener(this);
-		
-		if (mPager != null && mAdapter != null) initTabs();
+
+		if (mPager != null && mAdapter != null) {
+			initTabs();
+		}
 	}
-	
+
 	/**
 	 * Initialize and add all tabs to the layout
 	 */
 	private void initTabs() {
-		
+
 		removeAllViews();
 		mTabs.clear();
-		
-		if (mAdapter == null) return;
-		
+
+		if (mAdapter == null) {
+			return;
+		}
+
 		for (int i = 0; i < mPager.getAdapter().getCount(); i++) {
-			
+
 			final int index = i;
-			
+
 			View tab = mAdapter.getView(i);
 			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT, 1.0f);
 			tab.setLayoutParams(params);
 			this.addView(tab);
-			
+
 			mTabs.add(tab);
-			
+
 			if (i != mPager.getAdapter().getCount() - 1) {
 				this.addView(getSeparator());
 			}
-			
+
 			tab.setOnClickListener(new OnClickListener() {
 				@Override
 				public void onClick(View v) {
 					mPager.setCurrentItem(index);
 				}
 			});
-			
+
 		}
-		
+
 		selectTab(mPager.getCurrentItem());
 	}
-	
+
 	@Override
-	public void onPageScrollStateChanged(int state) {}
-	
+	public void onPageScrollStateChanged(int state) {
+		mOnPageChangeListener.onPageScrollStateChanged(state);
+	}
+
 	@Override
-	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
-	
+	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+		mOnPageChangeListener.onPageScrolled(position, positionOffset, positionOffsetPixels);
+	}
+
 	@Override
 	public void onPageSelected(int position) {
 		selectTab(position);
+		mOnPageChangeListener.onPageSelected(position);
 	}
-	
-	
+
 	/**
 	 * Creates and returns a new Separator View
 	 * 
@@ -154,34 +188,36 @@ public class FixedTabsView extends LinearLayout implements ViewPager.OnPageChang
 	 */
 	private View getSeparator() {
 		View v = new View(mContext);
-		
+
 		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(1, LayoutParams.FILL_PARENT);
 		params.setMargins(0, mDividerMarginTop, 0, mDividerMarginBottom);
 		v.setLayoutParams(params);
-		
-		if (mDividerDrawable != null) v.setBackgroundDrawable(mDividerDrawable);
-		else v.setBackgroundColor(mDividerColor);
-		
+
+		if (mDividerDrawable != null) {
+			v.setBackgroundDrawable(mDividerDrawable);
+		} else {
+			v.setBackgroundColor(mDividerColor);
+		}
+
 		return v;
 	}
-	
-	
+
 	/**
 	 * Runs through all tabs and sets if they are currently selected.
 	 * 
 	 * @param position
-	 *          The position of the currently selected tab.
+	 *            The position of the currently selected tab.
 	 */
 	private void selectTab(int position) {
-		
+
 		for (int i = 0, pos = 0; i < getChildCount(); i++) {
-			
+
 			if (this.getChildAt(i) instanceof ViewPagerTabButton) {
 				this.getChildAt(i).setSelected(pos == position);
 				pos++;
 			}
-			
+
 		}
 	}
-	
+
 }
--- src/com/astuetz/viewpager/extensions/ScrollingTabsView.java
+++ src/com/astuetz/viewpager/extensions/ScrollingTabsView.java
@@ -29,88 +29,114 @@ import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 
 public class ScrollingTabsView extends HorizontalScrollView implements OnPageChangeListener {
-	
+
 	@SuppressWarnings("unused")
 	private static final String TAG = "com.astuetz.viewpager.extensions";
-	
+
+	private static final OnPageChangeListener mDummyPageChangeListener = new OnPageChangeListener() {
+		@Override
+		public void onPageSelected(int arg0) {
+		}
+
+		@Override
+		public void onPageScrolled(int arg0, float arg1, int arg2) {
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int arg0) {
+		}
+	};
+
 	private Context mContext;
-	
+
 	private ViewPager mPager;
-	
+
 	private TabsAdapter mAdapter;
-	
+
 	private LinearLayout mContainer;
-	
+
 	private ArrayList<View> mTabs = new ArrayList<View>();
-	
+
 	private Drawable mDividerDrawable;
-	
+
 	private TabClickListener mClickListener;
-	
+
+	private OnPageChangeListener mOnPageChangeListener = mDummyPageChangeListener;
+
 	private int mDividerColor = 0xFF636363;
 	private int mDividerMarginTop = 12;
 	private int mDividerMarginBottom = 12;
 	private int mDividerWidth = 1;
-	
+
 	public ScrollingTabsView(Context context) {
 		this(context, null);
 	}
-	
+
 	public ScrollingTabsView(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
-	
+
 	public ScrollingTabsView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs);
-		
+
 		this.mContext = context;
 
 		mDividerMarginTop = (int) (getResources().getDisplayMetrics().density * mDividerMarginTop);
 		mDividerMarginBottom = (int) (getResources().getDisplayMetrics().density * mDividerMarginBottom);
-		mDividerWidth = (int) (getResources().getDisplayMetrics().density * mDividerWidth); 
-		
+		mDividerWidth = (int) (getResources().getDisplayMetrics().density * mDividerWidth);
+
 		final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ViewPagerExtensions, defStyle, 0);
-		
+
 		mDividerColor = a.getColor(R.styleable.ViewPagerExtensions_dividerColor, mDividerColor);
-		
+
 		mDividerMarginTop = a.getDimensionPixelSize(R.styleable.ViewPagerExtensions_dividerMarginTop, mDividerMarginTop);
-		mDividerMarginBottom = a.getDimensionPixelSize(R.styleable.ViewPagerExtensions_dividerMarginBottom,
-		    mDividerMarginBottom);
-		
+		mDividerMarginBottom = a.getDimensionPixelSize(R.styleable.ViewPagerExtensions_dividerMarginBottom, mDividerMarginBottom);
+
 		mDividerDrawable = a.getDrawable(R.styleable.ViewPagerExtensions_dividerDrawable);
-		
+
 		a.recycle();
-		
+
 		this.setHorizontalScrollBarEnabled(false);
 		this.setHorizontalFadingEdgeEnabled(false);
-		
+
 		mContainer = new LinearLayout(context);
 		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
 		mContainer.setLayoutParams(params);
 		mContainer.setOrientation(LinearLayout.HORIZONTAL);
-		
+
 		this.addView(mContainer);
-		
+
 	}
-	
+
+	public void setOnPageChangeListener(OnPageChangeListener listener) {
+		if (listener == null) {
+			mOnPageChangeListener = mDummyPageChangeListener;
+			return;
+		}
+
+		mOnPageChangeListener = listener;
+	}
+
 	public void setTabClickListener(TabClickListener listener) {
 		this.mClickListener = listener;
 	}
-	
+
 	/**
 	 * Sets the data behind this ScrollingTabsView.
 	 * 
 	 * @param adapter
-	 *          The {@link TabsAdapter} which is responsible for maintaining the
-	 *          data backing this FixedTabsView and for producing a view to
-	 *          represent an item in that data set.
+	 *            The {@link TabsAdapter} which is responsible for maintaining
+	 *            the data backing this FixedTabsView and for producing a view
+	 *            to represent an item in that data set.
 	 */
 	public void setAdapter(TabsAdapter adapter) {
 		this.mAdapter = adapter;
-		
-		if (mPager != null && mAdapter != null) initTabs();
+
+		if (mPager != null && mAdapter != null) {
+			initTabs();
+		}
 	}
-	
+
 	/**
 	 * Binds the {@link ViewPager} to this View
 	 * 
@@ -118,68 +144,83 @@ public class ScrollingTabsView extends HorizontalScrollView implements OnPageCha
 	public void setViewPager(ViewPager pager) {
 		this.mPager = pager;
 		mPager.setOnPageChangeListener(this);
-		
-		if (mPager != null && mAdapter != null) initTabs();
+
+		if (mPager != null && mAdapter != null) {
+			initTabs();
+		}
 	}
-	
+
 	/**
 	 * Initialize and add all tabs to the layout
 	 */
 	private void initTabs() {
-		
+
 		mContainer.removeAllViews();
 		mTabs.clear();
-		
-		if (mAdapter == null) return;
-		
+
+		if (mAdapter == null) {
+			return;
+		}
+
 		for (int i = 0; i < mPager.getAdapter().getCount(); i++) {
-			
+
 			final int index = i;
-			
+
 			View tab = mAdapter.getView(i);
 			mContainer.addView(tab);
-			
+
 			tab.setFocusable(true);
-			
+
 			mTabs.add(tab);
-			
+
 			if (i != mPager.getAdapter().getCount() - 1) {
 				mContainer.addView(getSeparator());
 			}
-			
+
 			tab.setOnClickListener(new OnClickListener() {
 				@Override
 				public void onClick(View v) {
-					if (mClickListener != null) mClickListener.onClick(index);
-					if (mPager.getCurrentItem() == index) selectTab(index);
-					else mPager.setCurrentItem(index);
+					if (mClickListener != null) {
+						mClickListener.onClick(index);
+					}
+					if (mPager.getCurrentItem() == index) {
+						selectTab(index);
+					} else {
+						mPager.setCurrentItem(index);
+					}
 				}
 			});
-			
+
 		}
-		
+
 		selectTab(mPager.getCurrentItem());
 	}
-	
+
 	@Override
-	public void onPageScrollStateChanged(int state) {}
-	
+	public void onPageScrollStateChanged(int state) {
+		mOnPageChangeListener.onPageScrollStateChanged(state);
+	}
+
 	@Override
-	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
-	
+	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+		mOnPageChangeListener.onPageScrolled(position, positionOffset, positionOffsetPixels);
+	}
+
 	@Override
 	public void onPageSelected(int position) {
 		selectTab(position);
+		mOnPageChangeListener.onPageSelected(position);
 	}
-	
-	
+
 	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
 		super.onLayout(changed, l, t, r, b);
-		
-		if (changed) selectTab(mPager.getCurrentItem());
+
+		if (changed) {
+			selectTab(mPager.getCurrentItem());
+		}
 	}
-	
+
 	/**
 	 * Creates and returns a new Separator View
 	 * 
@@ -187,48 +228,50 @@ public class ScrollingTabsView extends HorizontalScrollView implements OnPageCha
 	 */
 	private View getSeparator() {
 		View v = new View(mContext);
-		
+
 		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(mDividerWidth, LayoutParams.FILL_PARENT);
 		params.setMargins(0, mDividerMarginTop, 0, mDividerMarginBottom);
 		v.setLayoutParams(params);
-		
-		if (mDividerDrawable != null) v.setBackgroundDrawable(mDividerDrawable);
-		else v.setBackgroundColor(mDividerColor);
-		
+
+		if (mDividerDrawable != null) {
+			v.setBackgroundDrawable(mDividerDrawable);
+		} else {
+			v.setBackgroundColor(mDividerColor);
+		}
+
 		return v;
 	}
-	
-	
+
 	/**
 	 * Runs through all tabs and sets if they are currently selected.
 	 * 
 	 * @param position
-	 *          The position of the currently selected tab.
+	 *            The position of the currently selected tab.
 	 */
 	private void selectTab(int position) {
-		
+
 		for (int i = 0, pos = 0; i < mContainer.getChildCount(); i += 2, pos++) {
 			View tab = mContainer.getChildAt(i);
 			tab.setSelected(pos == position);
 		}
-		
+
 		View selectedTab = mContainer.getChildAt(position * 2);
-		
+
 		if (selectedTab != null) {
-		
+
 			final int w = selectedTab.getMeasuredWidth();
 			final int l = selectedTab.getLeft();
-			
+
 			final int x = l - this.getWidth() / 2 + w / 2;
-			
+
 			smoothScrollTo(x, this.getScrollY());
-		
+
 		}
-		
+
 	}
-	
+
 	public interface TabClickListener {
 		public void onClick(int position);
 	}
-	
+
 }
